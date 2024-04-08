8660e7610a89e462b13b32d36d5a4a165a80a22a
mav-dbt <mavdbt@gmail.com>
2024-04-08 23:43:31 +1200

    Add renderer to show 2D matrix using the p5.js library.

--- cellular-automata/src/main/resources/static/CellularAutomata.js
+++ cellular-automata/src/main/resources/static/CellularAutomata.js
@@ -2,29 +2,37 @@
 
 // Get the start button and define the canvas
 const startButton = document.getElementById("start-btn");
-const resolution = document.getElementById("resolution");
+const gridContainer = document.getElementById("grid-container");
+let rows = 10;
+let columns = 10;
+let resolution = 50;
 
 // Function to handle the start button click
 startButton.addEventListener("click", async () => {
-    const rows = document.getElementById("rows").value;
-    const columns = document.getElementById("columns").value;
-    const gridContainer = document.getElementById("grid-container");
+    rows = document.getElementById("rows").valueAsNumber;
+    columns = document.getElementById("columns").valueAsNumber;
+    resolution = document.getElementById("resolution").valueAsNumber;
     
+    // Create a dynamic canvas using the p5.js library
     createCanvas(columns * resolution, rows * resolution);
     // checkIfValid(canvas)
 
     const response = await fetch(`/api/cellular-automata/grid?rows=${rows}&columns=${columns}`);
     const grid = await response.json();
     console.table(grid);
-    draw();
+    draw(grid);
 });
 
-function draw() {
+function draw(grid) {
     background(0);
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
-            
-            
+            let y = i * resolution;
+            let x = j * resolution;
+            if (grid[i][j] == 1) {
+                fill(255);
+                rect(x, y, resolution, resolution);
+            }
         }
     }
 }
